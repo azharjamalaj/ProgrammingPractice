68adeeca3217f985a8ce9bc56d2216fcc7c5d86a
azharjamalaj <87015215+azharjamalaj@users.noreply.github.com>
2026-05-26 12:51:14 +0530

    Adding more Practice problem

--- src/main/java/TWOFIVEProgram/ReverseNumber.java
+++ src/main/java/TWOFIVEProgram/ReverseNumber.java
@@ -1,5 +1,21 @@
 package TWOFIVEProgram;
 
 public class ReverseNumber {
+    public static void main(String[] args) {
 
+        int num = 12345;
+        int result = 0;
+        int sum = 0;
+
+        while(num!=0)
+        {
+            int digit = num % 10 ;
+            result = result *10 +digit;
+            num= num/10;
+            sum = sum+digit;
+
+        }
+        System.out.println(result);
+        System.out.println(sum);
+    }
 }
